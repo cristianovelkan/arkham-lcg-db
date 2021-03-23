749ed6b07e0233ff69524d14673a5106964d1c1c
Cristiano <cristianovelkan@gmail.com>
2021-03-23 20:04:57 -0300

    feat: added user migration

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Cristiano',
+            'email' => 'cristianovelkan@gmail.com',
+            'password' => Hash::make("12345678"),
+            'is_admin' => true,
+            'is_verified' => true
+        ]);
+    }
+}
