32cf4acbb40eee9685ee0201d6f1aba17c9c150b
Cristiano <cristianovelkan@gmail.com>
2021-03-31 21:37:03 -0300

    feat: added user registered event & confirmation email

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -69,6 +69,11 @@ $app->singleton(
 $app->configure('app');
 $app->configure('auth');
 $app->configure('cors');
+$app->configure('mail');
+
+$app->alias('mailer', Illuminate\Mail\Mailer::class);
+$app->alias('mailer', Illuminate\Contracts\Mail\Mailer::class);
+$app->alias('mailer', Illuminate\Contracts\Mail\MailQueue::class);
 
 /*
 |--------------------------------------------------------------------------
@@ -104,6 +109,7 @@ $app->routeMiddleware([
 $app->register(App\Providers\AppServiceProvider::class);
 $app->register(App\Providers\AuthServiceProvider::class);
 $app->register(App\Providers\EventServiceProvider::class);
+$app->register(Illuminate\Mail\MailServiceProvider::class);
 
 $app->register(Fruitcake\Cors\CorsServiceProvider::class);
 $app->register(Laravel\Passport\PassportServiceProvider::class);
